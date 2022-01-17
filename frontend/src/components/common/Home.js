2d931b52726251c85ac38a42d9bf8dd2d233bc70
VanSHOE <rahulgargofficial@gmail.com>
2022-01-17 07:14:47 -0800

    Basic vendor food item dashboard

--- frontend/src/components/common/Home.js
+++ frontend/src/components/common/Home.js
@@ -1,5 +1,16 @@
+import * as React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import Paper from "@mui/material/Paper";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Rating from "@mui/material/Rating";
+import { styled } from "@mui/material/styles";
 const Home = (props) => {
   const [Items, setItems] = useState([]);
   const [details, setDetails] = useState([]);
@@ -23,10 +34,71 @@ const Home = (props) => {
       });
   }, []);
   console.log(Items);
+  const bull = (
+    <Box
+      component="span"
+      sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
+    >
+      •
+    </Box>
+  );
 
-  return Items.map((item) => (
-    <div style={{ textAlign: "center" }}>Happy {item.name} Cosding</div>
-  ));
+  const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  }));
+
+  return (
+    <Stack spacing={5}>
+      {Items.map((item) => (
+        <Box sx={{ minWidth: 275 }}>
+          <Card variant="outlined">
+            <React.Fragment>
+              <CardContent>
+                <Typography
+                  sx={{ fontSize: 14 }}
+                  color="text.secondary"
+                  gutterBottom
+                >
+                  {item.type}
+                </Typography>
+                <Typography variant="h5" component="div">
+                  {item.name}
+                </Typography>
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                  {item.price}$
+                </Typography>
+                <Typography variant="body2">
+                  <h4>Addons:</h4>
+                  <Stack direction="row" spacing={1}>
+                    {item.addons.map((addon) => (
+                      <Item>{addon}</Item>
+                    ))}
+                  </Stack>
+                </Typography>
+                <br />
+                <Rating name="read-only" value={item.rating} readOnly />
+                <Typography variant="body2">
+                  <h4>Tags:</h4>
+                  <Stack direction="row" spacing={1}>
+                    {item.tags.map((tag) => (
+                      <Item>{tag}</Item>
+                    ))}
+                  </Stack>
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">Edit</Button>
+                <Button size="small">Delete</Button>
+              </CardActions>
+            </React.Fragment>
+          </Card>
+        </Box>
+      ))}
+    </Stack>
+  );
 };
 
 export default Home;
