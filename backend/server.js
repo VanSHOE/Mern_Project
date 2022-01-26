2593c213057f0a531f960567e2d622c4de49f6a9
VanSHOE <rahulgargofficial@gmail.com>
2022-01-26 20:17:40 +0530

    Revert "Heroku req"
    This reverts commit 181bd965091d8116d1069360667b272e5c7e056a.

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,8 @@ const PORT = process.env.PORT || 5000;
 const path = require("path");
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../build")));
-  app.get("*", function (req, res) {
-    const index = path.join(__dirname, "build", "index.html");
-    res.sendFile(index);
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../build"));
   });
 }
 
